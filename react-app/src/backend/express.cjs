ac0211e11f8099fc885870740eb05f6fec1be31a
SagiBeno <benosagi321@gmail.com>
2025-09-22 08:13:59 +0200

    Handle custom error message

---
+++ react-app/src/backend/express.cjs
@@ -0,0 +1,14 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get((err, req, res) => {
+    if (err) res.status(404).send("<h1>404 Not Found (Hello world :) )</h1>")
+    else res.sendStatus(200)    
+})
+
+const port = 3333
+app.listen(port, err => console.log("Node Express backend server start; port, err: ", port, err))
